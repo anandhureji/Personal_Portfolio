82e64126d280caab313006ad46150f8b50381d49
Anandhu <anandhureji33@gmail.com>
2022-07-29 10:55:56 +0530

    about added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Intro from './components/introduction/Intro';
+import About from './components/about/About';
 
 function App() {
   return (
     
      <div>
        <Intro />
+       <About />
      </div>
     
   );
